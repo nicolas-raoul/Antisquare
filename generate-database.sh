600594d73762ab404900f37da77149ac0297036d
Harphuna <86975697+Hakunasc11@users.noreply.github.com>
2022-05-09 21:41:47 +0200

    Delete generate-database.sh

--- generate-database.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Generates a Java class that can tell which font(s) to use for any given character
-#
-java -cp bin:lib/sfntly_cvs20120313.jar fr.free.nrw.DatabaseGenerator > output.log
-echo "Generated: gen/fr/free/nrw/AntisquareData.java"
